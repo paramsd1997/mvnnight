e6ba0d37d1b0e5cf0f106eb3821b089077e030f2
parama <parama@gmail.com>
2022-08-25 22:34:20 +0530

    latest

---
+++ src/main/java/com/webservice/MvnApplication.java
@@ -0,0 +1,13 @@
+package com.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/webservice/Param.java
@@ -0,0 +1,14 @@
+package com.webservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Param {
+	
+	@GetMapping("/hello")
+public String newone() {
+		return "Hello boss";
+	}
+	
+}
