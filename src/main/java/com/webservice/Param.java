a025f5ecf911c786a0358ed97a9f058b7d4c743a
paramsd1997 <111410267+paramsd1997@users.noreply.github.com>
2022-08-26 15:30:59 +0530

    Update Param.java

--- src/main/java/com/webservice/Param.java
+++ src/main/java/com/webservice/Param.java
@@ -8,14 +8,7 @@ public class Param {
 	
 	@GetMapping("/hello")
 public String newone() {
-		return "Hello boss";
+		return "Hello Boss How are you";
 	}
-	@GetMapping("/hello1")
-public String newone() {
-		return "How Are You";
-	}
-	@GetMapping("/hello2")
-public String newone() {
-		return " I Am Good ";
-	}
+
 }
